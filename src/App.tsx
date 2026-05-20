df2bccdbd22a1ade7fad4b7654c8f7312631f767
oskar <ooskar@gmx.de>
2026-05-20 12:01:44 +0200

    feat: remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Projects from './pages/Projects';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<BrowserRouter>
-			<div id="page-container">
-				<Routes>
-					<Route path="/" element={<Projects />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <div id="page-container">
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
